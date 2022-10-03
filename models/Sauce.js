e3ce07686c9e520fe0b6fd013538aa5be2fb305f
simonamiel <sim.47photmail.fr>
2022-10-03 09:57:11 +0200

    Project P6 step 5 Finish sauce route of API end part

--- models/Sauce.js
+++ models/Sauce.js
@@ -8,8 +8,8 @@ const sauceShema = mongoose.Schema({
     mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true },
-    likes: { type: Number},
-    dislikes: { type: Number },
+    likes: { type: Number, default: 0},
+    dislikes: { type: Number, default: 0},
     usersLiked: { type: [String]},
     usersDisliked: { type: [String] },
 });
